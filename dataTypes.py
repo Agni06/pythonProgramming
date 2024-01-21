8c0f2ed9a2eef2823d3163d17784212a26336308
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-01-21 18:55:12 +0530

    Create dataTypes.py

---
+++ dataTypes.py
@@ -0,0 +1,10 @@
+print(3 * True)
+print(-3.1 * True)
+print(type("abc" * False))
+print(len("abc" * False))
+
+print(False + False)
+print(True + False)
+print(False + True)
+print(True + True)
+print(False + True + True + True)
